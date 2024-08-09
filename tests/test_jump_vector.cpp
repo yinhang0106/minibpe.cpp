cd6999b766df80bd8cb5a1deeb0c17d19de10c10
HangYin@work <yinhang0106@hotmail.com>
2024-08-09 19:35:43 +0800

    finish jump vector

--- tests/test_jump_vector.cpp
+++ tests/test_jump_vector.cpp
@@ -44,9 +44,10 @@ struct JumpVector {
     template <std::random_access_iterator InputIt>
     JumpVector(InputIt first, InputIt last) {
         size_t n = last - first;
+        std::cout << n << "\n";
         m_data = new Block[n];
         m_size = n;
-        for (size_t i = 0; i < n; i++) {
+        for (size_t i = 0; i < n - 1; i++) {
             m_data[i].m_next = &m_data[i + 1];
             m_data[i].m_value = *first;
             ++first;
@@ -64,44 +65,86 @@ struct JumpVector {
         return m_size;
     }
 
-    [[nodiscard]] Block* begin() const noexcept {
-        return m_data;
-    }
-
-    Block* operator++(const Block* block) const noexcept {
-        return block->m_next;
-    }
-
-    Block* operator+(Block* ptr, size_t n) const noexcept {
-        while (n > 0) {
-            ptr++;
-            --n;
-        }
-        return ptr;
-    }
-
     // read-write access
-    int& operator[](size_t index) const noexcept {
-        auto ptr = m_data + index;
+    [[nodiscard]] int& operator[](size_t index) const {
+        auto ptr = m_data;
+        while (index > 0) {
+            ptr = ptr->m_next;
+            index--;
+        }
         return ptr->m_value;
     }
 
+    // check if index is out of range!
+    [[nodiscard]] int& at(size_t index) const {
+        if (index >= m_size)
+            [[unlikely]] throw std::out_of_range("Index out of range");
+        auto ptr = m_data;
+        while (index > 0) {
+            ptr = ptr->m_next;
+            index--;
+        }
+        return ptr->m_value;
+    }
+
+
     void assign_and_shrink(size_t index, int val, size_t offset = 2) {
-        auto ptr = m_data + index;
+        if (index >= m_size)
+            [[unlikely]] throw std::out_of_range("Index out of range");
+
+        auto ptr = m_data;
+        while (index > 0) {
+            ptr = ptr->m_next;
+            index--;
+        }
         ptr->m_value = val;
-        ptr->m_next = ptr + offset - 1;
-        m_size -= offset - 1;
+
+        auto next = ptr->m_next;
+        while (offset > 1) {
+            next = next->m_next;
+            offset--;
+            m_size--;
+        }
+        ptr->m_next = next;
+    }
+
+    void find_assign_shrink(std::pair<int, int> val, int new_val) {
+        auto key = val.first;
+        auto next = val.second;
+        auto size = m_size;
+        auto ptr = m_data;
+
+        while (size > 1) {
+            if (ptr->m_value == key && ptr->m_next->m_value == next) {
+                ptr->m_value = new_val;
+                ptr->m_next = ptr->m_next->m_next;
+                m_size--;
+                size --;
+            }
+            ptr = ptr->m_next;
+            size--;
+        }
+    }
+
+    void print() const {
+        auto ptr = m_data;
+        while (ptr != nullptr) {
+            std::cout << ptr->m_value << " ";
+            ptr = ptr->m_next;
+        }
+        std::cout << "\n";
     }
 
 };
 
 
 int main() {
-    JumpVector jv({1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
-    jv.assign_and_shrink(3, 100, 2);
-    for (size_t i = 0; i < jv.size(); i++) {
-        std::cout << jv[i] << " ";
-    }
-    std::cout << "\n";
+    JumpVector jv({1, 2, 3, 4, 5, 6, 3, 3, 3, 4});
+    jv.print();
+    // jv.assign_and_shrink(2, 100, 3);
+    jv.find_assign_shrink({3, 3}, 100);
+    jv.print();
+    std::cout << jv.size() << "\n";
+
     return 0;
 }
